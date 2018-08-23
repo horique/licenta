b7adf63af7a12f699ac77f0a66ff232ccb5da734
Blebea Horia <blebeahoria@yahoo.com>
2018-08-23 18:35:29 +0300

    Update test.php

--- api/test.php
+++ api/test.php
@@ -1,14 +1,14 @@
 <?php
 
 echo ('hello' . _GET['file']);
-switch ($request[0]) {
-  case 'addEquipmentCategory':
-    echo ('hello world');
-  default:
-    print('NO METHOD');
-    http_response_code(404);
-    break;
-}
+// switch ($request[0]) {
+//   case 'addEquipmentCategory':
+//     echo ('hello world');
+//   default:
+//     print('NO METHOD');
+//     http_response_code(404);
+//     break;
+// }
 
 //not necessary
 $conn->close();
